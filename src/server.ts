eb43fbf10dd35cfafa79e6567725f8578bf936d4
Carol Patricio <87195745+carolina982@users.noreply.github.com>
2026-04-22 12:53:24 -0600

    correxion de perfil

--- src/server.ts
+++ src/server.ts
@@ -17,11 +17,13 @@ const app = express();
 const PORT =Number(process.env.PORT) || 3000;
 connectDB();
 
-app.use(cors ({
+app.use(cors({
     origin:"*",
-    methods:["GET","POST","PUT","DELETE","OPTIONS"],
+    methods:["GET","POST","PUT","PATCH","DELETE"],
     allowedHeaders:["Content-Type","Authorization"],
 }));
+
+
 
 app.options("*",cors());
 app.use(express.json());
