cf13ab4df7a8b471c88779929f9370342923324c
Carol Patricio <87195745+carolina982@users.noreply.github.com>
2026-05-15 10:36:24 -0600

    cambios de unidades

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,13 +1,14 @@
 import multer from "multer";
 import path from "path";
 
-const storage =multer.diskStorage({
-    destination:function(req,file,cd){
-        cd (null,"uploads");
+const storage=multer.diskStorage({
+    destination:function (req ,file, cd) {
+        cd (null,"/opt/render/project/src/uploads")
     },
-    filename:function (req,file,cd){
-        const ext=path.extname(file.originalname);
-        cd (null,Date.now()+ext);
+    filename : function(req ,file,cd){
+        const ext =path.extname(file.originalname);
+        cd (null, `${Date.now()}${ext}` );
     },
 });
-export const upload =multer({storage});
+
+export const upload=multer({storage});
