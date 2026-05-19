14b7c43a997dca5b203193398b3b3838188d589a
Carol Patricio <87195745+carolina982@users.noreply.github.com>
2026-05-19 13:07:42 -0600

    se agrego una api

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,7 +1,7 @@
-import multer from "multer";
-import path from "path";
+//import multer from "multer";
+//import path from "path";
 
-const storage=multer.diskStorage({
+/*const storage=multer.diskStorage({
     destination:function (req ,file, cd) {
         cd (null,"/opt/render/project/src/uploads")
     },
@@ -9,6 +9,18 @@ const storage=multer.diskStorage({
         const ext =path.extname(file.originalname);
         cd (null, `${Date.now()}${ext}` );
     },
+});*/
+
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary";
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params:async (req , file)=>({
+        folder:"units",
+        alloweb_formats:["jpg","jpeg","png"],
+    }),
 });
 
 export const upload=multer({storage});
