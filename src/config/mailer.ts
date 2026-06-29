10c277071a733b175f3bd6d752de5995704c1248
Carol Patricio <87195745+carolina982@users.noreply.github.com>
2026-06-29 13:20:41 -0600

    fix gmail mailer

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -1,15 +1,15 @@
 import nodemailer from "nodemailer";
+
 import { EMAIL_PASS, EMAIL_USER } from "./config";
 
-export const transporter=nodemailer.createTransport({
-    host:"smtp.gmail.com",
-    port:465,
-    secure:true,
-    auth:{
-        user:EMAIL_USER,
-        pass:EMAIL_PASS?.trim(),
-    },
-    tls:{
-        rejectUnauthorized:false,
-    },
-})
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  
+  auth: {
+    user: EMAIL_USER,
+    pass: EMAIL_PASS?.trim(),
+  },
+
+});
