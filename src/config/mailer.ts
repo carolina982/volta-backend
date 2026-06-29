f605aa10780c4394e1371e1391fcab575b03b551
Carol Patricio <87195745+carolina982@users.noreply.github.com>
2026-06-29 13:04:30 -0600

    cehcar el por que todavia sigue saliando error 06

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -3,8 +3,8 @@ import { EMAIL_PASS, EMAIL_USER } from "./config";
 
 export const transporter=nodemailer.createTransport({
     host:"smtp.gmail.com",
-    port:587,
-    secure:false,
+    port:465,
+    secure:true,
     auth:{
         user:EMAIL_USER,
         pass:EMAIL_PASS?.trim(),
