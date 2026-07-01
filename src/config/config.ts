20e626a1031d3429ba77c2a073cdaa4269099572
Carol Patricio <87195745+carolina982@users.noreply.github.com>
2026-07-01 12:51:21 -0600

    almacenimiento de imagenes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,17 @@
-
+import dotenv from "dotenv";
+dotenv.config();
 
 export const MONGO_URI ="mongodb://127.0.0.1:27017/volta";
 
-export const JWT_SECRET =process.env.JWT_SECRET || "mi_super_secreto";
+export const JWT_SECRET =process.env.JWT_SECRET || "dev_secret_change_me";
 
 
-
+//restablecer la contraseña 
 export const RESEND_API_KEY=process.env.RESEND_API_KEY ||"";
-export const EMAIL_FROM=process.env.EMAIL_FROM ||"onboarding@resend.dev"
+export const EMAIL_FROM=process.env.EMAIL_FROM ||"onboarding@resend.dev";
+
+//para aguardar imagenes
+
+export const CLOUDINARY_API_KEY=process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET=process.env.CLOUDINARY_API_SECRET;
+export const CLOUDINARY_CLOUD_NAME=process.env.CLOUDINARY_CLOUD_NAME;
